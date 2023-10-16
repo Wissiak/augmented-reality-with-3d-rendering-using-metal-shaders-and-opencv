18c97ea3d93fa44e43b33c1b633e38261e87b0a8
patrick.wissiak <patrick.wissiak@ost.ch>
2023-10-16 21:54:28 +0200

    Implement basic matcher

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/core/types.hpp>
+#include <opencv2/features2d.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/opencv.hpp>
-#include <opencv2/features2d.hpp>
 
 using std::cout;
 using std::endl;
@@ -15,40 +17,94 @@ cmake --build . --config Release
 */
 
 class ARWebcam {
-    public:
-        ARWebcam() {
-            
-        }
+private:
+  const cv::Mat referenceImage = cv::imread(
+      "/Users/patrick/tools/opencv-4.8.0/simple_demo/book1-reference.png", 0);
 
-    void video_in() {
-        cv::Mat myImage;
-        cv::namedWindow("Video Player");
-        cv::VideoCapture cap(0);
-        if (!cap.isOpened()) { 
-            cout << "No video stream detected" << endl;
-            system("pause");
-        }
-        while (true) {
-            cap >> myImage;
-            if (myImage.empty()) {
-            break;
-            }
-            imshow("Video Player", myImage);
-            char c = (char) cv::waitKey(1); 
+  std::vector<cv::KeyPoint> referenceKeypoints;
 
-            // If 'Esc' is entered break the loop           
-            if (c == 27) { 
-            break;
-            }
-        }
-        cap.release();
+  cv::Mat referenceDescriptors;
+  cv::Ptr<cv::SiftFeatureDetector> detector;
+  cv::Ptr<cv::BFMatcher> matcher;
+  std::vector<cv::Point2f> corners;
+
+public:
+  ARWebcam() {
+    detector = cv::SiftFeatureDetector::create(3000, 4, 0.001, 20, 1.5);
+    detector->detectAndCompute(referenceImage, cv::noArray(),
+                               referenceKeypoints, referenceDescriptors);
+    matcher = cv::BFMatcher::create(cv::NORM_L2, true);
+
+    unsigned height = referenceImage.rows;
+    unsigned width = referenceImage.cols;
+    unsigned data[8] = {0,         0,          width - 1, 0,
+                        width - 1, height - 1, 0,         height - 1};
+    
+    corners.push_back(cv::Point2f(0, 0));
+    corners.push_back(cv::Point2f(width-1, 0));
+    corners.push_back(cv::Point2f(width-1, height-1));
+    corners.push_back(cv::Point2f(0, height-1));
+  }
+
+  void video_in() {
+    cv::Mat videoFrame;
+    cv::namedWindow("Video Player");
+    cv::VideoCapture cap(0);
+    if (!cap.isOpened()) {
+      cout << "No video stream detected" << endl;
+      system("pause");
     }
+    while (true) {
+      cap >> videoFrame;
+      if (videoFrame.empty()) {
+        break;
+      }
+      std::vector<cv::KeyPoint> frameKeypoints;
+      cv::Mat frameDescriptors;
+      detector->detectAndCompute(videoFrame, cv::noArray(), frameKeypoints,
+                                 frameDescriptors);
+
+      std::vector<cv::DMatch> matches;
+      matcher->match(frameDescriptors, referenceDescriptors, matches,
+                     cv::Mat());
+
+      std::cout << matches.size() << std::endl;
+
+      if (matches.size() > 800) {
+        std::vector<cv::Point2f> obj;
+        std::vector<cv::Point2f> scene;
+        for (size_t i = 0; i < matches.size(); i++) {
+          obj.push_back(referenceKeypoints[matches[i].queryIdx].pt);
+          scene.push_back(frameKeypoints[matches[i].trainIdx].pt);
+        }
+
+        cv::Mat H = findHomography(obj, scene, cv::RANSAC, 5);
+
+        std::vector<cv::Point2f> transformedCorners;
+        cv::perspectiveTransform(corners, transformedCorners, H);
+
+        for (int i=0;i<transformedCorners.size();i++)
+            cv::circle(videoFrame,transformedCorners[i],10,CV_RGB(100,0,0),-1,8,0);
+        //cv::polylines(videoFrame, transformedCorners, true,
+        //              cv::Scalar(0, 255, 0), 3, cv::LINE_AA);
+
+      }
+      imshow("Video Player", videoFrame);
+      char c = (char)cv::waitKey(1);
+
+      // If 'Esc' is entered break the loop
+      if (c == 27) {
+        break;
+      }
+    }
+    cap.release();
+  }
 };
 
 int main(int argc, char **argv) {
 
-    ARWebcam arWebcam;
-    arWebcam.video_in();
+  ARWebcam arWebcam;
+  arWebcam.video_in();
 
-    return 0;
+  return 0;
 }
